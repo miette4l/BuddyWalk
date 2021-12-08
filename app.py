e54210466a9df576f1e78913495e1349b52c9d5a
holly <hhfarler@gmail.com>
2021-12-08 23:57:30 +0000

    Integrated, returns toy buddy

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, jsonify, request, render_template, redirect, url_for, session
+from users import return_buddy
 
 app = Flask(__name__)
-
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
 # note: Time of Departure is a str
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
+def display_form():
+    """Render the form"""
+    return render_template('form.html')
+
+
+@app.route('/', methods=['POST'])
 def user_input():
-    data = {}
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        # instead of print(data) call calc_route(data) and find_buddy(data)
-        print(data)
+    """ Grab and store user data from form, while checking if complete"""
+    data = request.form.to_dict()
+    session["data"] = data
 
-        # testing for user input in all fields:
-        missing = []
-        for k, v in data.items():
-            if v == "":
-                missing.append(k)
+    missing = []
+    for k, v in data.items():
+        if v == "":
+            missing.append(k)
 
-        if missing:
-            feedback = f"Missing fields for {', '.join(missing)}"
-            return render_template("form.html", feedback=feedback)
+    if missing:
+        feedback = f"Missing fields for {', '.join(missing)}"
+        return render_template("form.html", feedback=feedback)
 
-        return redirect(url_for('your_buddy'))
-    # do a redirect
-    return render_template('form.html', data=data)
+    return redirect(url_for('your_buddy'))
+    # do a redirect to result page
 
 
 @app.route('/yourbuddy', methods=['GET'])
 def your_buddy():
-    return 'Finding you a buddy'
+    return return_buddy(session["data"])
 
 
 if __name__ == '__main__':
