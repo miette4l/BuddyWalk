c23a3a860f59ed00a754361669a74150fde8d317
holly <hhfarler@gmail.com>
2021-12-13 00:31:22 +0000

    restructure

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session, send_file
-from users import return_buddy
+from find_buddy import find_buddy
+from SQL_DB_to_python_connect import add_journey_request
 
 app = Flask(__name__)
-app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
+app.secret_key = 'AIzaSyC6ShfxX_32v448NTO_xj-J9Wit9kNSLyg'
 
 # note: Time of Departure is a str
 
@@ -15,11 +16,15 @@ def display_form():
 
 @app.route('/', methods=['POST'])
 def user_input():
-    """ Grab and store user data from form, while checking if complete"""
+    """
+    Grab user data from form, while checking if complete
+    Save data as new record in DB
+    """
     data = {}
     if request.method == 'POST':
         data = request.form.to_dict()
-        session["data"] = data
+        session['current_user'] = data
+        add_journey_request(data['username'], data['CurrentLoc'], data['Destination'], data['ToD'])
 
     missing = []
     for k, v in data.items():
@@ -39,14 +44,15 @@ def your_buddy():
     """
     Finds and prints buddy's details, redirects to map with route on button click
     """
-    buddy_user = return_buddy(session["data"])  # returns a User type
-    # mock some data to fill the 'Your Buddy' template with:
-    # @V: I changed Name to be the buddy returned by the return_buddy function above
-    # so that is a real piece of buddy data
-    buddy = {'Username': buddy_user.username,
-             'Phone number': '12334556',
-             'Meeting Point': 'some address, some postcode',
-             'Meeting Time': '23:29 10/12/2021'}
+    buddy_username = find_buddy(session['current_user'])
+
+    buddy = {
+        'Username': buddy_username,
+        'Phone number': 'fake phonenumber',
+        'Meeting Point': 'fake spot',
+        'Time to meet': 'fake'
+    }
+
     if request.method == 'POST':
         return redirect(url_for('show_map'))
     return render_template('your_buddy.html', buddy=buddy)
@@ -56,7 +62,7 @@ def your_buddy():
 def show_map():
     """Shows map with route"""
     f_map = 'f_map.html'  # replace with backend logic to generate map
-    return send_file(f'{f_map}') # opens up interactive map
+    return send_file(f'{f_map}')  # opens up interactive map
 
 
 if __name__ == '__main__':
